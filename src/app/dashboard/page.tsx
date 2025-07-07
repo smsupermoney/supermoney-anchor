33c8aa74f9a37f570fd0c0a91269ccd3cb810306
Nitin Chorge <nitin.chorge@supermoney.in>
2025-07-07 10:55:20 +0000

    left arrow button corousel not getting hide on not hovered

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -182,8 +182,8 @@ export default function Dashboard() {
                   </Card>
               </CarouselItem>
           </CarouselContent>
-          <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 -translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
-          <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
+          <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 disabled:opacity-0 -translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
+          <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 disabled:opacity-0 translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
         </Carousel>
       </div>
       
@@ -247,8 +247,8 @@ export default function Dashboard() {
                 );
               })}
             </CarouselContent>
-            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 -translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
-            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
+            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 disabled:opacity-0 -translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
+            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 hidden md:flex hover:bg-primary hover:text-primary-foreground opacity-0 group-hover:opacity-100 disabled:opacity-0 translate-x-8 group-hover:translate-x-0 transition-all duration-300" />
           </Carousel>
         </div>
       </div>
