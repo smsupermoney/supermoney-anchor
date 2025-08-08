13eb7cc8473b0947e5873c028bfdfd8d93b2cfaa
Nitin Chorge <nitin.chorge@supermoney.in>
2025-08-08 08:44:09 +0000

    Again mail is not sent to dealer You are fetching emailAdress and it is

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -144,7 +144,7 @@ export async function getDealers(anchorId?: string): Promise<Dealer[]> {
         return {
             id: dealerId,
             name: dealerData.dealerName,
-            email: userEmailMap.get(dealerId) || undefined,
+            email: userEmailMap.get(dealerId),
             anchorId: dealerData.anchorId,
             programId: dealerData.programId,
             applicationId: dealerData.applicationId,
@@ -496,6 +496,7 @@ export const dealerLeads: DealerLead[] = [
     
 
     
+
 
 
 
