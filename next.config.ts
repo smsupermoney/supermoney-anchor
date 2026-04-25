3d932f801c6a13c7beb60dc1a1b9220ba4cab1a4
Yash Jain <yash.jain@supermoney.in>
2026-04-25 10:27:55 +0000

    4|anchor-supermoney  |  ⚠ Invalid next.config.ts options detected: 4|an

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  serverActions: {
-    bodySizeLimit: '50mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
   typescript: {
     ignoreBuildErrors: true,
